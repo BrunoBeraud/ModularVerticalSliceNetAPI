58dcfb126c1a7c5fbce38979b5ce466f7f23c896
reborn <bazzite@2a02-8429-5890-2901-c9be-057f-ef95-4950.rev.sfr.net>
2025-01-23 19:00:52 +0100

    observability extension fixes (code format, set oltp endpoint)

--- src/Host/ObservabilityBuilderExtension.cs
+++ src/Host/ObservabilityBuilderExtension.cs
@@ -1,4 +1,5 @@
 using OpenTelemetry;
+using OpenTelemetry.Exporter;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -18,28 +19,23 @@ internal static class ObservabilityBuilderExtension
 
         var otel = builder.Services.AddOpenTelemetry();
 
-        var OtlpServiceName = builder.Configuration["OpenTelemetry:OTELSERVICE_NAME"];
-        ArgumentNullException.ThrowIfNull(OtlpServiceName);
-        otel.ConfigureResource(builder => builder.AddService(serviceName: OtlpServiceName));
+        var otlpServiceName = builder.Configuration["OpenTelemetry:OTELSERVICE_NAME"];
+        ArgumentNullException.ThrowIfNull(otlpServiceName);
+        otel.ConfigureResource(builder => builder.AddService(serviceName: otlpServiceName));
 
         // Add Metrics for ASP.NET Core and export via OTLP
         otel.WithMetrics(metrics =>
-        {
             // Metrics provider from OpenTelemetry
-            metrics.AddAspNetCoreInstrumentation();
-        });
+            metrics.AddAspNetCoreInstrumentation());
 
         // Add Tracing for ASP.NET Core and export via OTLP
-        otel.WithTracing(tracing =>
-        {
-            tracing.AddAspNetCoreInstrumentation();
-        });
+        otel.WithTracing(tracing => tracing.AddAspNetCoreInstrumentation());
 
         // Export OpenTelemetry data via OTLP, using env vars for the configuration
-        var OtlpEndpoint = builder.Configuration["OpenTelemetry:OTEL_EXPORTER_OTLP_ENDPOINT"];
-        ArgumentNullException.ThrowIfNull(OtlpEndpoint);
+        var otlpEndpoint = builder.Configuration["OpenTelemetry:OTEL_EXPORTER_OTLP_ENDPOINT"];
+        ArgumentNullException.ThrowIfNull(otlpEndpoint);
 
-        otel.UseOtlpExporter();
+        otel.UseOtlpExporter(OtlpExportProtocol.Grpc, new Uri(otlpEndpoint));
 
         return builder;
     }
